4b1280829c5730a89c93769792d58fa919d4db2c
yein <gpg7913@gmail.com>
2022-06-30 00:28:07 +0900

    (create) kakao 인가코드 넘겨주기 추가, route 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,24 @@ import { Routes, Route } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import Main from "@pages/Main";
 import Login from "@pages/Login";
+import NotFound from "@pages/NotFound";
+import Oauth from "@pages/Oauth";
+import Mypage from "@pages/Mypage";
 import GlobalStyle from './styles/GlobalStyle';
 
 library.add(fas);
-
 function App() {
   return (
     <RecoilRoot>
       <GlobalStyle />
       <Routes>
-        <Route path='/' element={<Login />} />
+        <Route path='/' element={<Main />} />
+        <Route path='/login' element={<Login />}/>
+        <Route path='/mypage' element={<Mypage />}/>
+        <Route path='/user/oauth/:social' element={<Oauth />}/>
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </RecoilRoot>
   );
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-function Login(){
-  return(
+function Login() {
+  return (
     <div>
-      <button type="button">
+      <a
+        href="https://kauth.kakao.com/oauth/authorize?client_id=1db0c5eb9c2fdd2ba5412540e5d9a37b&redirect_uri=
+http://localhost:12345/user/oauth/kakao&response_type=code">
         <img src="/ui/kakao_login_medium_narrow.png" alt="카카오 로그인 버튼"/>
-      </button>
+      </a>
     </div>
   )
 }
+
 export default Login
