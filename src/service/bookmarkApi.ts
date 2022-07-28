a452ce916791005fb71b425142b07c183f7806b8
calvin9150 <wonbae9150@gmail.com>
2022-07-29 03:35:27 +0900

    (Implement) 폴더명 수정 및 api 수정사항 반영

--- src/service/bookmarkApi.ts
+++ src/service/bookmarkApi.ts
@@ -8,15 +8,15 @@ import { request } from './httpClient';
 
 export const getUserBookmarkFolders = () =>
   request<GetUserBookmarkFoldersResponse[]>({
-    method: 'POST',
+    method: 'GET',
     url: '/folderList',
   });
 
 export const createBookmarkFolder = (body: { folderName: string }) =>
   request<CreateBookmarkFolderResponse>({ method: 'POST', url: '/folder', data: body });
 
-export const updateBookmarkFolderName = (params: { folderId: number }, body: { folderName: string }) =>
-  request({ method: 'PATCH', url: '/bookmark', params, data: body });
+export const updateBookmarkFolderName = ({ folderId }: { folderId: number }, params: { newFolderName: string }) =>
+  request({ method: 'PUT', url: `/folderUpdate/${folderId}`, params });
 
 export const deleteBookmarkFolder = (params: number) => request({ method: 'DELETE', url: `/folder/${params}` });
 
