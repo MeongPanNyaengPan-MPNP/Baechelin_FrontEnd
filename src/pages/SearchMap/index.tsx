2fef0ba0d699df79654ec2bc48b9490159c65320
yein <gpg7913@gmail.com>
2022-08-03 01:11:04 +0900

    (improve) 지도페이지에서도 위치 동의하도록

--- src/pages/SearchMap/index.tsx
+++ src/pages/SearchMap/index.tsx
@@ -2,8 +2,20 @@
 import React from 'react';
 import MapWrapper from '@organisms/MapWrapper';
 import { STORE_FILTERS } from '@constants/store';
+import UseGeolocation from '@hooks/UseGeolocation';
+import { useRecoilState } from 'recoil';
+import locationAtom from '@recoil/locationAtom';
 
 function SearchMap() {
+  const { currentLocation } = UseGeolocation();
+  const [location, setLocation] = useRecoilState(locationAtom);
+
+  React.useEffect(() => {
+    if (currentLocation !== null && location === null) {
+      // localstorage 초기 셋팅
+      setLocation(currentLocation);
+    }
+  }, [currentLocation]);
   return <MapWrapper filters={STORE_FILTERS} />;
 }
 
